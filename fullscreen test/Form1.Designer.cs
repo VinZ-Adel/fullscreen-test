00f892b8628ed1a8f73a289d9e9d706958d9fd4c
Vinnie <vin.zedigh@adelfors.nu>
2024-03-12 15:09:50 +0100

    BLUH

--- fullscreen test/Form1.Designer.cs
+++ fullscreen test/Form1.Designer.cs
@@ -32,6 +32,11 @@
 			button1 = new Button();
 			label1 = new Label();
 			timer1 = new System.Windows.Forms.Timer(components);
+			button2 = new Button();
+			button3 = new Button();
+			button4 = new Button();
+			listBox1 = new ListBox();
+			button5 = new Button();
 			SuspendLayout();
 			// 
 			// button1
@@ -60,11 +65,65 @@
 			timer1.Interval = 1000;
 			timer1.Tick += timer1_Tick;
 			// 
+			// button2
+			// 
+			button2.Location = new Point(326, 264);
+			button2.Name = "button2";
+			button2.Size = new Size(75, 23);
+			button2.TabIndex = 2;
+			button2.Text = "topmost";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += topmost;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(585, 143);
+			button3.Name = "button3";
+			button3.Size = new Size(75, 23);
+			button3.TabIndex = 3;
+			button3.Text = "button3";
+			button3.UseVisualStyleBackColor = true;
+			button3.Click += button3_Click;
+			// 
+			// button4
+			// 
+			button4.Location = new Point(525, 331);
+			button4.Name = "button4";
+			button4.Size = new Size(115, 59);
+			button4.TabIndex = 4;
+			button4.Text = "Topmost simple";
+			button4.UseVisualStyleBackColor = true;
+			button4.Click += topmost_simple;
+			// 
+			// listBox1
+			// 
+			listBox1.FormattingEnabled = true;
+			listBox1.ItemHeight = 15;
+			listBox1.Location = new Point(12, 41);
+			listBox1.Name = "listBox1";
+			listBox1.Size = new Size(120, 349);
+			listBox1.TabIndex = 5;
+			// 
+			// button5
+			// 
+			button5.Location = new Point(639, 37);
+			button5.Name = "button5";
+			button5.Size = new Size(75, 23);
+			button5.TabIndex = 6;
+			button5.Text = "reset";
+			button5.UseVisualStyleBackColor = true;
+			button5.Click += button5_Click;
+			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(button5);
+			Controls.Add(listBox1);
+			Controls.Add(button4);
+			Controls.Add(button3);
+			Controls.Add(button2);
 			Controls.Add(label1);
 			Controls.Add(button1);
 			Name = "Form1";
@@ -79,5 +138,10 @@
 		private Button button1;
 		private Label label1;
 		private System.Windows.Forms.Timer timer1;
+		private Button button2;
+		private Button button3;
+		private Button button4;
+		private ListBox listBox1;
+		private Button button5;
 	}
 }
--- fullscreen test/Form1.cs
+++ fullscreen test/Form1.cs
@@ -1,19 +1,20 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace fullscreen_test
 {
 	public partial class Form1 : Form
 	{
-		List<Process> GamesAndMenu = [];
+		List<KeyValuePair<Process, IntPtr>> GamesAndMenuWithHandles = [];
 		bool KeepFocus = true;
 		public Form1()
 		{
 			InitializeComponent();
 			//this.FormBorderStyle = FormBorderStyle.None;
 			//this.WindowState = FormWindowState.Maximized;
-			GamesAndMenu.Add(Process.GetCurrentProcess()); // Adds this to process list, should be in position [0]
+			GamesAndMenuWithHandles.Add(new(Process.GetCurrentProcess(), this.Handle)); // Adds this to process list, should be in position [0]
 		}
 
 		private void button1_Click(object sender, EventArgs e)
@@ -23,7 +24,8 @@ namespace fullscreen_test
 			label1.Text = path;
 			try
 			{
-				GamesAndMenu.Add(Process.Start(path + @"\" + game)); // starts the indicated game and adds it to process list
+				Process temp = Process.Start(path + @"\" + game);       // starts the indicated game and adds it to process list
+				GamesAndMenuWithHandles.Add(new(temp, temp.MainWindowHandle));  // ^
 			}
 			catch { label1.Text = "Error"; }
 		}
@@ -43,38 +45,149 @@ namespace fullscreen_test
 		internal static extern IntPtr SetForegroundWindow(IntPtr hWnd);
 
 		[DllImport("user32.dll")]
-		static extern nint GetForegroundWindow();
+		static extern IntPtr GetForegroundWindow();
 
 		[DllImport("kernel32.dll")]
 		static extern int GetProcessId(IntPtr handle);
 
 		[DllImport("user32.dll")]
-		public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+		public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out nint a);
+
+		[DllImport("user32.dll")]
+		public static extern bool AttachThreadInput(int idAttach, int idAttatchTo, bool fAttatch);
+
+		[DllImport("user32.dll")]
+		public static extern bool BringWindowToTop(IntPtr hWnd);
+
+
+		[DllImport("user32.dll")]
+		static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+		static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+		const UInt32 SWP_NOSIZE = 0x0001;
+		const UInt32 SWP_NOMOVE = 0x0002;
+		const UInt32 SWP_SHOWWINDOW = 0x0040;
 
 		private void timer1_Tick(object sender, EventArgs e)
 		{
 			if (KeepFocus)
 			{
-				List<Process> remove = [];
-				foreach (Process p in GamesAndMenu)
+				List<KeyValuePair<Process, IntPtr>> remove = [];
+				foreach (KeyValuePair<Process, IntPtr> p in GamesAndMenuWithHandles)
 				{
-					if (p.HasExited)
+					if (p.Key.HasExited)
 						remove.Add(p);
 				}
-				foreach (Process p in remove.Intersect(GamesAndMenu))
-					GamesAndMenu.Remove(p);
+				foreach (KeyValuePair<Process, IntPtr> p in remove)
+					GamesAndMenuWithHandles.Remove(p);
 
-				uint pid;
-				GetWindowThreadProcessId(GetForegroundWindow(), out pid);
-				Process currentFocus = Process.GetProcessById((int)pid);
-				if (currentFocus != GamesAndMenu[^1])
+				IntPtr bluh = GetForegroundWindow();
+				IntPtr test = this.Handle;
+
+				if (GamesAndMenuWithHandles.Any(c => c.Value == bluh)) { label1.Text = "same"; }
+				else if (GamesAndMenuWithHandles.Any(c => c.Key.MainWindowHandle == bluh)) { label1.Text = "SAME"; }
+				else
+					label1.Text = "not";
+
+
+				Process[] processesrunning = Process.GetProcesses();
+				foreach (Process process in processesrunning)
 				{
-					IntPtr hWnd = GamesAndMenu[^1].Handle;
-					ShowWindow(hWnd, 3);
-					SetForegroundWindow(hWnd);
-					GetWindowThreadProcessId(GetForegroundWindow(), out pid);
+					if (process.MainWindowHandle == bluh)
+					{
+						AttachThreadInput(Process.GetCurrentProcess().Id, process.Id, true);
+						IntPtr hWnd = GamesAndMenuWithHandles[^1].Key.MainWindowHandle; // THIS WORkS (.Key.MainWindowHandle), .Value DOES NOT
+						BringWindowToTop(hWnd);
+						AttachThreadInput(Process.GetCurrentProcess().Id, process.Id, false);
+					}
+				}
+
+				//nint parentid;
+				//nint pid;
+				//pid = GetWindowThreadProcessId(GetForegroundWindow(), out parentid);
+				//Process currentFocus = Process.GetProcessById((int)pid);
+				//if (currentFocus != GamesAndMenuWithHandles[^1].Key) //should be !GamesAndMenu.Contains(currentFocus) in real one
+				//{
+				//	AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)pid, true);
+				//	IntPtr hWnd = GamesAndMenuWithHandles[^1].Value;
+				//	BringWindowToTop(hWnd);
+				//	//ShowWindow(hWnd, 3);
+				//	//SetWindowPos(hWnd, -1, 0, 0, 0, 0, 0x0002 | 0x0001); // sets as topmost
+				//	AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)pid, false);
+				//}
+			}
+			else
+			{
+				foreach (KeyValuePair<Process, IntPtr> pro in GamesAndMenuWithHandles)
+					SetWindowPos(pro.Value, -2, 0, 0, 0, 0, 0x0002 | 0x0001);
+			} // removes topmost
+		}
+
+		private void topmost(object sender, EventArgs e) // still does not work
+		{
+			nint parentid;
+			nint pid;
+			IntPtr bluh = GetForegroundWindow();
+			IntPtr test = this.Handle;
+
+			if (bluh == test) { label1.Text = "same"; }
+
+
+			Process[] processesrunning = Process.GetProcesses();
+			foreach (Process process in processesrunning)
+			{
+				if (process.MainWindowHandle == bluh)
+				{
+					AttachThreadInput(Process.GetCurrentProcess().Id, process.Id, true);
+					IntPtr hWnd = GamesAndMenuWithHandles[^1].Value;
+					BringWindowToTop(hWnd);
+					AttachThreadInput(Process.GetCurrentProcess().Id, process.Id, false);
 				}
 			}
+			//try
+			//{
+			//	pid = GetWindowThreadProcessId(bluh, out parentid);
+
+			//	if (pid != 0)
+			//	{
+			//		AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)pid, true);
+			//	}
+			//	else
+			//	{
+			//		AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)parentid, true);
+			//	}
+
+			//	IntPtr hWnd = GamesAndMenuWithHandles[^1].Value;
+			//	BringWindowToTop(hWnd);
+			//	//bool test = SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+			//	if (pid != 0 )
+			//		AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)pid, false);
+			//	else
+			//		AttachThreadInput(GamesAndMenuWithHandles[0].Key.Id, (int)parentid, false);
+			//}
+			//catch { label1.Text = "Id get error"; }
+		}
+
+		// Import user32.dll (containing the function we need) and define
+		// the method corresponding to the native function.
+		[DllImport("user32.dll")]
+		private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			// Invoke the function as a regular managed method.
+			MessageBox(IntPtr.Zero, "Command-line message box", "Attention!", 0);
+		}
+
+		private void topmost_simple(object sender, EventArgs e)
+		{
+			SetWindowPos(GamesAndMenuWithHandles[^1].Value, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			foreach (KeyValuePair<Process, IntPtr> pro in GamesAndMenuWithHandles)
+				SetWindowPos(pro.Value, -2, 0, 0, 0, 0, 0x0002 | 0x0001);
 		}
 	}
 }
